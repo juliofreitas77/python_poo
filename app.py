5c9a5a18698d2258772aca04b7e527b069dd0ea0
Júlio César <julio.si.freitas@gmail.com>
2024-09-20 23:41:26 -0300

    Importando Classes

---
+++ app.py
@@ -0,0 +1,13 @@
+from modelos.retaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_japones.alternar_estado()
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
