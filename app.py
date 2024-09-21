cd6e3f5b00b71a6b801887279ea3dcb975331c5f
Júlio César <julio.si.freitas@gmail.com>
2024-09-21 00:27:05 -0300

    Composição - Consolidando o conhecimento

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from modelos.retaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
+restaurante_praca.receber_avaliacao('Julio', 10)
+restaurante_praca.receber_avaliacao('Jose', 4)
+restaurante_praca.receber_avaliacao('Ana', 5)
+#restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+#restaurante_japones = Restaurante('Japa', 'Japonesa')
 
-restaurante_japones.alternar_estado()
+#restaurante_japones.alternar_estado()
+
 
 def main():
     Restaurante.listar_restaurantes()
